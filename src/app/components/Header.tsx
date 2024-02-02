4c3d47e94d7bf7723837b67c15f82965605fa806
Uchenna Njoku <uchen@MacBook-Pro-6.local>
2024-02-02 04:35:42 -0500

    First Batch

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+    return (
+        <header className='bg-white max-w-full sticky top-0 z-10'>
+            <div className="flex w-screen justify-between items-center m-auto max-w-8xl p-10">
+                <div className="flex-1"></div> {/* Invisible spacer */}
+                <div className="flex flex-grow justify-center items-center space-x-10"> {/* Centered content without forcing width */}
+                    <Link href="/experience" className='text-black no-underline border-b border-transparent transition-all hover:border-current'>experience</Link>
+                    <Link href="/works" className='text-black no-underline border-b border-transparent transition-all hover:border-current'>works</Link>
+                    <Link href="/contact" className='text-black no-underline border-b border-transparent transition-all hover:border-current'>contact</Link>
+                </div>
+                <div className='flex-1 flex justify-end'> {/* Right-aligned content */}
+                    <Link href="/" className='border-2 pointer hover:bg-gray-600 hover:text-white transition-all border-gray-800 rounded-md px-2 py-1 text-gray-800'>get my resume</Link>
+                </div>
+            </div>
+        </header>
+    
+    );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,47 @@
+import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-white relative w-screen">
+      <header className='bg-white w-full  absolute top-0 z-10 p-10'>
+            <div className="flex w-full justify-between items-center m-auto max-w-8xl">
+                <div className="flex-1"></div> {/* Invisible spacer */}
+                <div className="flex flex-grow justify-center items-center space-x-10"> {/* Centered content without forcing width */}
+                    <Link href="/experience" className='text-black no-underline border-b border-transparent transition-all hover:border-current'>/experience</Link>
+                    <Link href="/works" className='text-black no-underline border-b border-transparent transition-all hover:border-current'>/works</Link>
+                    <Link href="/contact" className='text-black no-underline border-b border-transparent transition-all hover:border-current'>/contact</Link>
+                </div>
+                <div className='flex-1 flex justify-end'> {/* Right-aligned content */}
+                    <Link href="/" className='border-2 pointer hover:bg-gray-600 hover:text-white transition-all border-gray-800 rounded-md px-2 py-1 text-gray-800'>get my resume</Link>
+                </div>
+            </div>
+      </header>
+      <section className="z-0 flex flex-col m-auto justify-center h-screen max-w-5xl text-black -pt-[120px]">
+        <div >
+
+          <h1 className="text-8xl m-0">Hi,<span role="img" className='waveanim' aria-label="sheep">👋🏼</span> I'm <span className="line-through"><b>your next software developer,</b></span> <b>Uchenna Njoku</b></h1>
+          <h3 className="text-2xl m-0">specializing in crafting <b>unique digital experiences. </b></h3>
+        
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <section className="flex flex-col h-screen m-auto justify-center  max-w-5xl text-black">
+        
+        <div className="flex flex-row">
+          <div className="flex flex-col space-y-5">
+            <h2 className="text-5xl m-0">I'm a software engineer based in Daytona Beach, Florida</h2>
+            <p className="text-2xl m-0">I have a knack for full-stack development, clean design patterns, human-computer interactions, and everything in between. I'm quietly confident, naturally curious, and perpetually improving my chops one problem at a time!</p>
+            <p className="text-2xl font-medium m-0">previously worked at:</p>
+            <ul className="text-2xl m-0">
+              <li>🚀 <a href="https://www.amazon.com/" target="_blank">Amazon</a></li>
+              <li>🔥 <a href="https://www.https://avtcseries.org/about-the-ecocar-ev-challenge/" target="_blank">General Motors EV Challenge</a></li>
+            </ul>
+          </div>  
+        </div>
+      </section>
+    </main> 
   );
 }
